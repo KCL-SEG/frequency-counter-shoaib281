9799949ad9213f8c927f36122297a51a2da43949
MOHAMMAD (SHOAIB) SHOAIB <mohammad.shoaib@kcl.ac.uk>
2023-12-08 11:14:18 +0000

    Done

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,10 @@
 
 def frequencies(items):
     frequencies = {}
+    for item in items:
+        item = str(item)
+        if item not in frequencies:
+            frequencies[item] = 0
+        frequencies[item] += 1
     # Your code goes here
     return frequencies
